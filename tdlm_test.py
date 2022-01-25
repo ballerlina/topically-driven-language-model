486c6811809ba149fa4bfca8a6c81b9573bfd855
Carolina Zheng <swirlinq@gmail.com>
2022-01-25 19:00:32 +0000

    Update test to work with my preprocessing

--- tdlm_test.py
+++ tdlm_test.py
@@ -2,6 +2,7 @@
 Stdin:          N/A
 Stdout:         N/A
 Author:         Jey Han Lau
+Modified by:    Carolina Zheng
 Date:           Oct 16
 """
 
@@ -16,8 +17,10 @@ import tensorflow as tf
 from collections import namedtuple
 from tdlm_model import TopicModel as TM
 from tdlm_model import LanguageModel as LM
-from util import pad, get_batch, get_batch_doc, gen_data, print_corpus_stats
+from util import get_batch_doc
 from gensim import matutils
+from ltlm_data import process_dataset, get_batch_v2, PAD, SOS, EOS
+from ltlm_util import Config
 
 #parser arguments
 desc = "Given a trained TDLM model, perform various test inferences"
@@ -53,7 +56,7 @@ debug = False
 #functions#
 ###########
 
-def compute_dt_dist(docs, labels, tags, model, max_len, batch_size, pad_id, idxvocab, output_file):
+def compute_dt_dist(docs, labels, tags, model, max_len, batch_size, pad_id, vocab, output_file):
     #generate batches
     num_batches = int(math.ceil(float(len(docs)) / batch_size))
     dt_dist = []
@@ -68,7 +71,7 @@ def compute_dt_dist(docs, labels, tags, model, max_len, batch_size, pad_id, idxv
         if debug:
             for si in xrange(s):
                 d = x[si]
-                print "\n\nDoc", docid, "=", " ".join([idxvocab[item] for item in d if (item != pad_id)])
+                print "\n\nDoc", docid, "=", " ".join([vocab.get_word(item) for item in d if (item != pad_id)])
                 sorted_dist = matutils.argsort(attention[si], reverse=True)
                 for ti in sorted_dist:
                     print "Topic", ti, "=", attention[si][ti]
@@ -76,34 +79,58 @@ def compute_dt_dist(docs, labels, tags, model, max_len, batch_size, pad_id, idxv
 
     np.save(open(output_file, "w"), dt_dist)
 
-def run_epoch(sents, docs, tags, (tm, lm), pad_id, cf, idxvocab):
-    #generate the batches
-    tm_num_batches, lm_num_batches = int(math.ceil(float(len(sents[0]))/cf.batch_size)), \
-        int(math.ceil(float(len(sents[1]))/cf.batch_size))
-
-    #run an epoch to compute tm and lm perplexities
+def run_epoch(data, (tm, lm), pad_id, cf, ltlm_cf):
+    docs_segmented, doc_bows, doc_num_tokens = data
+    batch_idx = 0
+    prev_doc_idxs = None
+    prev_sequence_idxs = None
+    prev_running_bows = np.zeros((cf.batch_size, num_tm_words))
+    lm_costs, lm_words = 0.0, 0.0
     if tm != None:
         tm_costs, tm_words = 0.0, 0.0
-        for bi in xrange(tm_num_batches):
-            _, y, m, d, t = get_batch(sents[0], docs[0], tags, bi, cf.doc_len, cf.tm_sent_len, cf.tag_len, cf.batch_size, \
-                pad_id, False)
-            tm_cost = sess.run(tm.tm_cost, {tm.y: y, tm.tm_mask: m, tm.doc: d, tm.tag: t})
-            tm_costs += tm_cost * cf.batch_size
-            tm_words += np.sum(m)
+
+    while True:
+        (
+            data,
+            bows,
+            target,
+            mask,
+            prev_doc_idxs,
+            prev_sequence_idxs,
+            is_last_sequence,
+        ) = get_batch_v2(
+            ltlm_cf,
+            cf.batch_size,
+            docs_segmented,
+            doc_bows,
+            prev_doc_idxs,
+            prev_sequence_idxs,
+            prev_running_bows,
+        )
+        if data is None:
+            break
+        batch_idx += 1
+
+        feed_dict = {lm.x: data, lm.y: target, lm.lm_mask: mask}
+        if cf.topic_number > 0:
+            feed_dict.update({lm.doc: bows, lm.tag: None})
+
+        lm_cost = sess.run(lm.lm_cost, feed_dict)
+        lm_costs += lm_cost * cf.batch_size
+        lm_words += np.sum(mask)
+
+        if tm != None:
+            # TODO: Compute TM ppl
+            pass
+
+    print("Num batches: {}".format(batch_idx))
+    print "test language model perplexity = %.3f" % (np.exp(lm_costs/lm_words))
+
+    if tm != None:
         print "\ntest topic model perplexity = %.3f" % (np.exp(tm_costs/tm_words))
 
-    if lm != None:
-        lm_costs, lm_words = 0.0, 0.0
-        for bi in xrange(lm_num_batches):
-            x, y, m, d, t = get_batch(sents[1], docs[1], tags, bi, cf.doc_len, cf.lm_sent_len, cf.tag_len, cf.batch_size, \
-                pad_id, True)
-            lm_cost, tw = sess.run([lm.lm_cost, lm.tm_weights], {lm.x: x, lm.y: y, lm.lm_mask: m, lm.doc: d, lm.tag: t})
-            lm_costs += lm_cost * cf.batch_size
-            lm_words += np.sum(m)
 
-        print "test language model perplexity = %.3f" % (np.exp(lm_costs/lm_words))
-
-def run_epoch_doc(docs, labels, tags, tm, pad_id, cf):
+def run_epoch_doc(docs, labels, tm, pad_id, cf):
     batches = int(math.ceil(float(len(docs))/cf.batch_size))
     accs = []
     for b in xrange(batches):
@@ -114,36 +141,36 @@ def run_epoch_doc(docs, labels, tags, tm, pad_id, cf):
 
     print "\ntest classification accuracy = %.3f" % np.mean(accs)
 
-def gen_sent_on_topic(idxvocab, vocabxid, start_symbol, end_symbol, cf):
+def gen_sent_on_topic(vocab, start_symbol, end_symbol, cf):
     output = codecs.open(args.gen_sent_on_topic, "w", "utf-8")
     topics, entropy = tm.get_topics(sess, topn=topn)
     with tf.variable_scope("model", reuse=True, initializer=initializer):
-        mgen = LM(is_training=False, vocab_size=len(idxvocab), batch_size=1, num_steps=1, config=cf, \
+        mgen = LM(is_training=False, vocab_size=len(vocab), batch_size=1, num_steps=1, config=cf, \
             reuse_conv_variables=True)
 
     for t in range(cf.topic_number):
         output.write("\n" + "="*100 + "\n")
         output.write("Topic " +  str(t) + ":\n")
-        output.write(" ".join([ idxvocab[item] for item in topics[t] ]) + "\n\n")
+        output.write(" ".join([ vocab.get_word(item) for item in topics[t] ]) + "\n\n")
 
         output.write("\nSentence generation (greedy; argmax):" + "\n")
-        s = mgen.generate_on_topic(sess, t, vocabxid[start_symbol], 0, cf.lm_sent_len+10, vocabxid[end_symbol])
-        output.write("[0] " + " ".join([ idxvocab[item] for item in s ]) + "\n")
+        s = mgen.generate_on_topic(sess, t, vocab[start_symbol], 0, cf.lm_sent_len+10, vocabxid[end_symbol])
+        output.write("[0] " + " ".join([ vocab.get_word(item) for item in s ]) + "\n")
         
         for temp in gen_temps:
             output.write("\nSentence generation (random; temperature = " + str(temp) + "):\n")
             for i in xrange(gen_num):
-                s = mgen.generate_on_topic(sess, t, vocabxid[start_symbol], temp, cf.lm_sent_len+10, \
-                    vocabxid[end_symbol])
-                output.write("[" + str(i) + "] " +  " ".join([ idxvocab[item] for item in s ]) + "\n")
+                s = mgen.generate_on_topic(sess, t, vocab[start_symbol], temp, cf.lm_sent_len+10, \
+                    vocab[end_symbol])
+                output.write("[" + str(i) + "] " +  " ".join([ vocab.get_word(item) for item in s ]) + "\n")
 
-def gen_sent_on_doc(docs, tags, idxvocab, vocabxid, start_symbol, end_symbol, cf):
+def gen_sent_on_doc(docs, tags, vocab, start_symbol, end_symbol, cf):
     topics, _ = tm.get_topics(sess, topn=topn)
-    topics = [ " ".join([idxvocab[w] for w in t]) for t in topics ]
+    topics = [ " ".join([vocab.get_word(w) for w in t]) for t in topics ]
     doc_text = [ item.replace("\t", "\n") for item in codecs.open(args.input_doc, "r", "utf-8").readlines() ]
     output = codecs.open(args.gen_sent_on_doc, "w", "utf-8")
     with tf.variable_scope("model", reuse=True, initializer=initializer):
-        mgen = LM(is_training=False, vocab_size=len(idxvocab), batch_size=1, num_steps=1, config=cf, \
+        mgen = LM(is_training=False, vocab_size=len(vocab), batch_size=1, num_steps=1, config=cf, \
             reuse_conv_variables=True)
 
     for d in range(len(docs)):
@@ -159,28 +186,26 @@ def gen_sent_on_doc(docs, tags, idxvocab, vocabxid, start_symbol, end_symbol, cf
             for item in best_topics ]) + "\n")
 
         output.write("\nRepresentative words:\n")
-        output.write("\n".join([ ("[%.3f] %s" % (item[1], idxvocab[item[0]])) for item in best_words ]) + "\n")
+        output.write("\n".join([ ("[%.3f] %s" % (item[1], vocab.get_word(item[0]))) for item in best_words ]) + "\n")
 
         output.write("\nSentence generation (greedy; argmax):" + "\n")
-        s = mgen.generate_on_doc(sess, doc, t, vocabxid[start_symbol], 0, cf.lm_sent_len+10, vocabxid[end_symbol])
-        output.write("[0] " + " ".join([ idxvocab[item] for item in s ]) + "\n")
+        s = mgen.generate_on_doc(sess, doc, t, vocab[start_symbol], 0, cf.lm_sent_len+10, vocab[end_symbol])
+        output.write("[0] " + " ".join([ vocab.get_word(item) for item in s ]) + "\n")
 
         for temp in gen_temps:
             output.write("\nSentence generation (random; temperature = " + str(temp) + "):\n")
 
             for i in xrange(gen_num):
-                s = mgen.generate_on_doc(sess, doc, t, vocabxid[start_symbol], temp, cf.lm_sent_len+10, \
-                    vocabxid[end_symbol])
-                output.write("[" + str(i) + "] " + " ".join([ idxvocab[item] for item in s ]) + "\n")
+                s = mgen.generate_on_doc(sess, doc, t, vocab[start_symbol], temp, cf.lm_sent_len+10, \
+                    vocab[end_symbol])
+                output.write("[" + str(i) + "] " + " ".join([ vocab.get_word(item) for item in s ]) + "\n")
 ######
 #main#
 ######
 
 #load the vocabulary
-vocab = cPickle.load(open(os.path.join(args.model_dir, "vocab.pickle")))
-idxvocab, tm_ignore, dummy_symbols = vocab[0], vocab[1], vocab[2]
-pad_symbol, start_symbol, end_symbol = dummy_symbols[0], dummy_symbols[1], dummy_symbols[2]
-vocabxid = dict([(y,x) for x,y in enumerate(idxvocab)])
+vocab, num_tm_words = cPickle.load(open(os.path.join(args.model_dir, "vocab.pickle")))
+pad_symbol, start_symbol, end_symbol = PAD, SOS, EOS
 
 #load config
 cf_dict = cPickle.load(open(os.path.join(args.model_dir, "config.pickle")))
@@ -194,39 +219,45 @@ ModelConfig = namedtuple("ModelConfig", " ".join(cf_dict.keys()))
 cf = ModelConfig(**cf_dict)
 
 #parse and collect the documents
-if args.input_doc:
-    sents, docs, docids, stats = gen_data(vocabxid, dummy_symbols, tm_ignore, args.input_doc, \
-        cf.tm_sent_len, cf.lm_sent_len, cf.verbose, False)
-    #print documents statistics
-    print "Vocab size =", len(idxvocab)
-    print_corpus_stats("Documents statistics", sents, docs, stats)
-
-#collect the labels
-#labels given for documents
-if args.input_label:
-    labels = [ int(item) for item in open(args.input_label).readlines() ]
+# if args.input_doc:
+#     sents, docs, docids, stats = gen_data(vocab, dummy_symbols, tm_ignore, args.input_doc, \
+#         cf.tm_sent_len, cf.lm_sent_len, cf.verbose, False)
+if cf.stopwords is not None:
+    with open(cf.stopwords, "r") as f:
+        stopwords = set(f.read().splitlines())
 else:
-    labels = None
+    stopwords = {}
 
-#collect the tags
-if cf.num_tags > 0:
-    if not args.input_tag:
-        sys.stderr.write("Error: Saved model is trained with document tags; " + \
-            "please specify tag metadata using --input_tag\n")
-        raise SystemExit
+config_attrs = {
+    "model_type": "TDLM",
+    "num_tm_words": num_tm_words,
+    "max_seqlen": cf.lm_sent_len,
+    "pad_idx": vocab[PAD],
+    "use_all_bows": False,
+    "eval_false": True,
+    "reset_hidden": True,
+}
+ltlm_cf = Config(**config_attrs)
 
-    tagxid = cPickle.load(open(os.path.join(args.model_dir, "tag.pickle")))
-    tags = [ [ tagxid[t] for t in line.strip().split("\t") if t in tagxid ] \
-        for line in codecs.open(args.input_tag, "r", "utf-8").readlines() ]
-else:
-    tags = None
+_, _, test_data, vocab, num_tm_words = process_dataset(
+    stopwords,
+    cf.data_path,
+    cf.lm_sent_len,
+    cf.doc_len,
+    reproduce_tdlm=True,
+    vocab=vocab,
+    test_only=True,
+)
+print "Vocab size =", len(vocab)
+
+labels, tags = None, None
 
 with tf.Graph().as_default(), tf.Session() as sess:
     initializer = tf.contrib.layers.xavier_initializer(seed=cf.seed)
     with tf.variable_scope("model", reuse=None, initializer=initializer):
-        tm = TM(is_training=False, vocab_size=len(idxvocab), batch_size=cf.batch_size, \
+        tm = TM(is_training=False, vocab_size=len(vocab), batch_size=cf.batch_size, \
             num_steps=cf.tm_sent_len, num_classes=cf.num_classes, config=cf) if cf.topic_number > 0 else None
-        lm = LM(is_training=False, vocab_size=len(idxvocab), batch_size=cf.batch_size, \
+        lm = LM(is_training=False, vocab_size=len(vocab), batch_size=cf.batch_size, \
             num_steps=cf.lm_sent_len, config=cf, reuse_conv_variables=True) if cf.rnn_hidden_size > 0 else None
 
     #load tensorflow model
@@ -238,7 +269,8 @@ with tf.Graph().as_default(), tf.Session() as sess:
         if args.input_doc == None:
             sys.stderr.write("Error: --output_topic_dist option requires --input_doc\n")
             raise SystemExit
-        compute_dt_dist(docs[0], labels, tags, tm, cf.doc_len, cf.batch_size, vocabxid[pad_symbol], idxvocab, \
+        # TODO: Fix
+        compute_dt_dist(docs[0], labels, tags, tm, cf.doc_len, cf.batch_size, vocab[pad_symbol], vocab, \
             args.output_topic_dist)
 
     #print topics
@@ -246,39 +278,23 @@ with tf.Graph().as_default(), tf.Session() as sess:
         topics, entropy = tm.get_topics(sess, topn=topn)
         output = codecs.open(args.output_topic, "w", "utf-8")
         for ti, t in enumerate(topics):
-            output.write(" ".join([ idxvocab[item] for item in t ]) + "\n")
-
-    #output tag embeddings
-    if args.output_tag_embedding and cf.num_tags > 0:
-        np.save(open(args.output_tag_embedding, "w"), sess.run(tm.tag_embedding))
+            output.write(" ".join([ vocab.get_word(item) for item in t ]) + "\n")
 
     #compute test perplexities
     if args.print_perplexity:
         if args.input_doc == None:
             sys.stderr.write("Error: --print_perplexity requires --input_doc\n")
             raise SystemExit
-        run_epoch(sents, docs, tags, (tm, lm), vocabxid[pad_symbol], cf, idxvocab)
-
-    #compute classification accuracy
-    if args.print_acc:
-        if tm == None:
-            sys.stderr.write("Error: Saved model does not have a topic model component; --print_acc ignored\n")
-            raise SystemExit
-        if cf.num_classes == 0:
-            sys.stderr.write("Error: Saved topic model is not a supervised topic model; --print_acc ignored\n")
-            raise SystemExit
-        if labels == None or args.input_doc == None:
-            sys.stderr.write("Error: --print_acc requires --input_doc and --input_label\n")
-            raise SystemExit
-        run_epoch_doc(docs[0], labels, tags, tm, vocabxid[pad_symbol], cf)
+        run_epoch(test_data, (tm, lm), vocab[pad_symbol], cf, ltlm_cf)
 
     #generate sentences conditioned on topics
     if args.gen_sent_on_topic:
-        gen_sent_on_topic(idxvocab, vocabxid, start_symbol, end_symbol, cf)
+        gen_sent_on_topic(vocab, start_symbol, end_symbol, cf)
 
     #generate sentences conditioned on documents
     if args.gen_sent_on_doc:
         if args.input_doc == None:
             sys.stderr.write("Error: --gen_sent_on_doc option requires --input_doc\n")
             raise SystemExit
-        gen_sent_on_doc(docs[0], tags, idxvocab, vocabxid, start_symbol, end_symbol, cf)
+        # TODO: Fix
+        gen_sent_on_doc(docs[0], tags, vocab, start_symbol, end_symbol, cf)
